b6f34f87fb87c372c6fdd8e7670f7a206cdcb4f0
pysush <32834855+pysush@users.noreply.github.com>
2018-01-23 16:05:53 +0530

    codes files

--- Chapter08/hillar_django_restful_08_01/scripts/create_user.py
+++
@@ -1,5 +0,0 @@
-from django.contrib.auth.models import User
-
-user = User.objects.create_user('user01', 'user01@example.com','user01password')
-user.save()
-
--- Chapter10/hillar_django_restful_10_01/restful01/toys/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Book: Django RESTful Web Services
-Author: Gaston C. Hillar - Twitter.com/gastonhillar
-Publisher: Packt Publishing Ltd. - http://www.packtpub.com
-"""
